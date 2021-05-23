04d2e88669a43ac309b3dafe956e1a39e0382e05
Daniel Kim <kkdh1109@gmail.com>
2021-05-23 13:13:20 -0400

    create the states to store the user's choice

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './UploadProductPage.css';
 
-function UploadProductPage() {
+const Continents = [
+    { key: 1, value: "Africa" },
+    { key: 2, value: "Europe" },
+    { key: 3, value: "Asia" },
+    { key: 4, value: "North America" },
+    { key: 5, value: "South America" },
+    { key: 6, value: "Australia" },
+    { key: 7, value: "Antarctica" }
+]
 
-    
+function UploadProductPage() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState(0);
+    const [continent, setContinent] = useState(1);
+
     return (
         <div className='upload'>
             <div className='heading'>
@@ -17,7 +30,7 @@ function UploadProductPage() {
 
                 <label>Title</label>
                 <input
-                    onChange={onTitleChange}
+                    onChange={e => setTitle(e.target.value)}
                     value={title}
                 />
                 
@@ -26,8 +39,8 @@ function UploadProductPage() {
 
                 <label>Description</label>
                 <textarea
-                    onChange
-                    value
+                    onChange={e => setDescription(e.target.value)}
+                    value={description}
                 />
                 
                 <br />
@@ -35,15 +48,18 @@ function UploadProductPage() {
 
                 <label>Price($)</label>
                 <input
-                    onChange
-                    value
+                    onChange={e => setPrice(e.target.value)}
+                    value={price}
                     type='number'
                 />
 
-                <select>
-                    <option key value>
-
-                    </option>
+                <select
+                    onChange={e => setContinent(e.target.value)}
+                >
+                    {Continents.map(item => (
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
+                    
                 </select>
                 
                 <br />
